e705489107a2ef11b0688e8fa027722f2dd47a24
Ethan Hellyer <ethanhellyer@Ethans-MacBook-Air.local>
2026-03-15 07:44:30 +1100

    reverted advanced matrix change

--- Content/Items/Accessories/AdvancedCircuitMatrix.cs
+++ Content/Items/Accessories/AdvancedCircuitMatrix.cs
@@ -18,6 +18,7 @@ public class AdvancedCircuitMatrix : ModItem {
     public override void UpdateAccessory(Player player, bool hideVisual) {
         var omp = player.GetModPlayer<OmnitrixPlayer>();
         omp.advancedCircuitMatrix =  true;
-        omp.omnitrixEnergyRegen   += 500;
+        // This simply exists for my dev needs
+        // omp.omnitrixEnergyRegen   += 500;
     }
 }
