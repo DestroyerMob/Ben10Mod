d58d1131c34856b984de3a1e62dc50b89d8d4540
DestroyerMob <ethanhellyer7@gmail.com>
2026-03-14 22:31:11 +1100

    Changed XLR8 to use an ability instead of an attack

--- Content/Transformations/XLR8/XLR8Transformation.cs
+++ Content/Transformations/XLR8/XLR8Transformation.cs
@@ -2,20 +2,25 @@ using System;
 using Ben10Mod.Content.Buffs.Abilities;
 using Ben10Mod.Content.Projectiles;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Ben10Mod.Content.Transformations.XLR8;
 
 public class XLR8Transformation : Transformation {
-    public override string FullID => "Ben10Mod:XLR8";
-    public override string TransformationName => "XLR8";
-    public override string IconPath => "Ben10Mod/Content/Interface/XLR8Select";
-    public override int TransformationBuffId => ModContent.BuffType<XLR8_Buff>();
-    public override int PrimaryAbilityDuration => 10 * 60;
-    public override int PrimaryAttack => ModContent.ProjectileType<FistProjectile>();
-    public override int PrimaryAttackSpeed => 3;
-    public override int PrimaryShootSpeed => 30;
+    public override string FullID                  => "Ben10Mod:XLR8";
+    public override string TransformationName      => "XLR8";
+    public override string IconPath                => "Ben10Mod/Content/Interface/XLR8Select";
+    public override int    TransformationBuffId    => ModContent.BuffType<XLR8_Buff>();
+    public override int    PrimaryAbilityDuration  => 10 * 60;
+    public override int    PrimaryAttack           => ModContent.ProjectileType<FistProjectile>();
+    public override int    PrimaryAttackSpeed      => 3;
+    public override int    PrimaryShootSpeed       => 30;
+    public override bool   HasUltimateAbility      => true;
+    public override int    UltimateAbilityCost     => 100;
+    public override int    UltimateAbilityDuration => 4 * 60;
+    public override int    UltimateAbilityCooldown => 60 * 60;
 
     public override void UpdateEffects(Player player, OmnitrixPlayer omp) {
         base.UpdateEffects(player, omp);
@@ -31,7 +36,7 @@ public class XLR8Transformation : Transformation {
 
     public override void FrameEffects(Player player, OmnitrixPlayer omp) {
 
-        player.armorEffectDrawShadowEOCShield = true;
+        
         
         var costume = ModContent.GetInstance<XLR8>();
         player.head = EquipLoader.GetEquipSlot(Mod, costume.Name, EquipType.Head);
@@ -40,5 +45,10 @@ public class XLR8Transformation : Transformation {
         player.body = EquipLoader.GetEquipSlot(Mod, costume.Name, EquipType.Body);
         player.legs = EquipLoader.GetEquipSlot(Mod, costume.Name, EquipType.Legs);
         player.waist = EquipLoader.GetEquipSlot(Mod, costume.Name, EquipType.Waist);
+    }
+
+    public override void ModifyDrawInfo(Player player, OmnitrixPlayer omp, ref PlayerDrawSet drawInfo) {
+        base.ModifyDrawInfo(player, omp, ref drawInfo);
+        player.armorEffectDrawShadowEOCShield = true;
     }
 }
