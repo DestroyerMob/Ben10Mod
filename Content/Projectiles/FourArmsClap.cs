b1525c8f401406636c17b3ec8a53660cc7714120
DestroyerMob <ethanhellyer7@gmail.com>
2026-03-23 08:35:10 +1100

    Fixed effect

--- Content/Projectiles/FourArmsClap.cs
+++ Content/Projectiles/FourArmsClap.cs
@@ -1,46 +1,74 @@
-﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Ben10Mod.Content.Projectiles {
-    public class FourArmsClap : ModProjectile {
+namespace Ben10Mod.Content.Projectiles;
 
-        public override string Texture => $"Terraria/Images/Projectile_{ProjectileID.None}";
+public class FourArmsClap : ModProjectile {
+    private const int ImpactDustCount = 56;
+    private const float ImpactSpread = 1.08f;
+    private const float ImpactSurfaceInset = 4f;
 
-        public override void SetDefaults() {
-            Projectile.width = 8;
-            Projectile.height = 8;
-            Projectile.aiStyle = ProjAIStyleID.Arrow;
+    public override string Texture => $"Terraria/Images/Projectile_{ProjectileID.None}";
 
-            AIType                 = ProjectileID.Bullet;
-            Projectile.friendly    = true;
-            Projectile.timeLeft    = 360;
-            Projectile.tileCollide = false;
-            Projectile.DamageType  = DamageClass.Ranged;
+    public override void SetDefaults() {
+        Projectile.width = 8;
+        Projectile.height = 8;
+        Projectile.aiStyle = ProjAIStyleID.Arrow;
+
+        AIType = ProjectileID.Bullet;
+        Projectile.friendly = true;
+        Projectile.timeLeft = 360;
+        Projectile.tileCollide = false;
+        Projectile.DamageType = DamageClass.Ranged;
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+        Vector2 impactDirection = Projectile.velocity.SafeNormalize(Vector2.UnitX);
+        Vector2 burstDirection = -impactDirection;
+        Vector2 impactPoint = GetImpactPoint(target, impactDirection);
+
+        SpawnImpactBurst(impactPoint, burstDirection);
+    }
+
+    public override void EmitEnchantmentVisualsAt(Vector2 boxPosition, int boxWidth, int boxHeight) {
+        for (int i = 0; i < 16; i++) {
+            int dustNum = Dust.NewDust(boxPosition, boxWidth, boxHeight, DustID.WhiteTorch);
+            Main.dust[dustNum].noGravity = true;
         }
+    }
 
-        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
-            for (int i = 0; i < 64; i++) {
-                int dustNum = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.WhiteTorch,  Main.rand.NextFloat(-85, 85), Main.rand.NextFloat(-85, 85), Scale: 3);
-                Main.dust[dustNum].noGravity = true;
+    private void SpawnImpactBurst(Vector2 impactPoint, Vector2 burstDirection) {
+        for (int i = 0; i < ImpactDustCount; i++) {
+            float spread = Main.rand.NextFloat(-ImpactSpread, ImpactSpread);
+            Vector2 direction = burstDirection.RotatedBy(spread).SafeNormalize(burstDirection);
+            float speed = Main.rand.NextFloat(5.5f, 11.5f);
+            Vector2 velocity = direction * speed;
+            Vector2 position = impactPoint + direction * Main.rand.NextFloat(0f, 8f);
+
+            Dust dust = Dust.NewDustPerfect(position, DustID.WhiteTorch, velocity, 80, Color.White,
+                Main.rand.NextFloat(1.6f, 2.7f));
+            dust.noGravity = true;
+
+            if (Main.rand.NextBool(3)) {
+                dust.velocity += burstDirection * Main.rand.NextFloat(0.8f, 1.6f);
             }
         }
 
-        public override void EmitEnchantmentVisualsAt(Vector2 boxPosition, int boxWidth, int boxHeight) {
-            Random random = new Random();
-            for (int i = 0; i < 16; i++) {
-                int dustNum = Dust.NewDust(boxPosition, boxWidth, boxHeight, DustID.WhiteTorch);
-                Main.dust[dustNum].noGravity = true;
-            }
+        for (int i = 0; i < 12; i++) {
+            Vector2 direction = burstDirection.RotatedBy(Main.rand.NextFloat(-0.42f, 0.42f)).SafeNormalize(burstDirection);
+            Dust coreDust = Dust.NewDustPerfect(impactPoint, DustID.Smoke, direction * Main.rand.NextFloat(1.2f, 3.2f), 110,
+                new Color(240, 240, 240), Main.rand.NextFloat(1.1f, 1.6f));
+            coreDust.noGravity = true;
         }
+    }
 
+    private Vector2 GetImpactPoint(NPC target, Vector2 impactDirection) {
+        Rectangle hitbox = target.Hitbox;
+        Vector2 surfacePoint = new(
+            MathHelper.Clamp(Projectile.Center.X, hitbox.Left, hitbox.Right),
+            MathHelper.Clamp(Projectile.Center.Y, hitbox.Top, hitbox.Bottom));
+        return surfacePoint - impactDirection * ImpactSurfaceInset;
     }
 }
