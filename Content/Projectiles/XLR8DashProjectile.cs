ee917164fdb96e91ffa6c2d2c4a139241ae1fe58
DestroyerMob <ethanhellyer7@gmail.com>
2026-03-23 19:25:24 +1100

    Changed it more to a ram

--- Content/Projectiles/XLR8DashProjectile.cs
+++ Content/Projectiles/XLR8DashProjectile.cs
@@ -1,9 +1,13 @@
 using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 
 namespace Ben10Mod.Content.Projectiles;
 
 public class XLR8DashProjectile : RathPounceProjectile {
+    protected override float DashSpeed => 18f;
+    protected override float DashLift => -0.4f;
+    protected override float ForwardOffset => 30f;
     protected override int HitDebuffType => 0;
     protected override int HitDebuffDuration => 0;
     protected override Color OuterColor => new(16, 24, 42, 220);
@@ -13,4 +17,20 @@ public class XLR8DashProjectile : RathPounceProjectile {
     protected override int PrimaryImpactDustType => DustID.BlueCrystalShard;
     protected override int SecondaryImpactDustType => DustID.BlueCrystalShard;
     protected override Color ImpactDustColor => new(145, 225, 255);
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+        base.OnHitNPC(target, hit, damageDone);
+
+        Player owner = Main.player[Projectile.owner];
+        if (owner.active && !owner.dead) {
+            Vector2 dashDirection = Projectile.rotation.ToRotationVector2();
+            owner.velocity = -dashDirection * 6.5f + new Vector2(0f, -0.9f);
+            owner.immune = true;
+            owner.immuneNoBlink = true;
+            owner.immuneTime = System.Math.Max(owner.immuneTime, 12);
+            owner.noKnockback = false;
+        }
+
+        Projectile.Kill();
+    }
 }
