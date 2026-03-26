98571e6f3ecd059dce462829b1fc332db6a33e2e
DestroyerMob <ethanhellyer7@gmail.com>
2026-03-26 16:09:47 +1100

    Localization

---
+++ Content/Transformations/Anodite/Anodite.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Ben10Mod.Content.Items.Placeables;
+
+namespace Ben10Mod.Content.Transformations.Anodite
+{
+    public class Anodite : ModItem {
+        public static string TransformationDescription =>
+            "A Pyronite built for constant pressure. Heat Blast controls fire, shrugs off lava, and can erupt into a devastating supernova while burning everything nearby.";
+
+        public static IReadOnlyList<string> TransformationAbilities => new[] {
+            "Primary ability: deploy a flare rod sentry that scorches nearby enemies.",
+            "Secondary ability: unleash a supernova heat wave that expands from your body and ignites everything it touches.",
+            "Main attack: rapid flamethrower stream.",
+            "Alt attack: explosive fire bomb.",
+            "Passive: lava immunity, fire walking, and fiery melee hits.",
+            "Mobility: gains a powerful flame-propelled extra jump while transformed.",
+            "Ultimate attack: sustained high-power fire blast."
+        };
+
+        public override void Load() {
+            if (Main.netMode == NetmodeID.Server)
+                return;
+
+            EquipLoader.AddEquipTexture(Mod, $"{Texture}_{EquipType.Head}", EquipType.Head, this, equipTexture: new XLR8Head());
+            EquipLoader.AddEquipTexture(Mod, $"{Texture}_{EquipType.Body}", EquipType.Body, this);
+            EquipLoader.AddEquipTexture(Mod, $"{Texture}_{EquipType.Legs}", EquipType.Legs, this);
+        }
+
+        private void SetupDrawing() {
+            if (Main.netMode == NetmodeID.Server)
+                return;
+
+            int equipSlotHead = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head);
+            int equipSlotBody = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Body);
+            int equipSlotLegs = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Legs);
+
+            ArmorIDs.Head.Sets.DrawHead[equipSlotHead] = false;
+            ArmorIDs.Body.Sets.HidesTopSkin[equipSlotBody] = true;
+            ArmorIDs.Body.Sets.HidesArms[equipSlotBody] = true;
+            ArmorIDs.Legs.Sets.HidesBottomSkin[equipSlotLegs] = true;
+        }
+
+        public override void SetStaticDefaults() {
+            SetupDrawing();
+        }
+
+        public override void SetDefaults() {
+            Item.width = 40;
+            Item.height = 80;
+            Item.useAnimation = 30;
+            Item.useTime = 30;
+            Item.useStyle = ItemUseStyleID.HiddenAnimation;
+            Item.consumable = true;
+        }
+    }
+
+    public class XLR8Head : EquipTexture {
+        public override bool IsVanitySet(int head, int body, int legs) => true;
+    }
+}
